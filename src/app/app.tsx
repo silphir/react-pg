6d31d1af7c60531e0c81d2596f1950a1ac956fcf
Kwark Donghyuk <silphir@hanmail.net>
2020-06-01 10:21:17 +0900

    feat(init): CRLF

---
+++ src/app/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import './app.scss';
+
+function App() {
+  const Div = styled.div`
+    text-align: center;
+  `;
+  
+  return (
+    <Div />
+  );
+}
+
+export default App;

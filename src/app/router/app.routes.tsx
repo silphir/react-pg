f03956829cb35ca988abb0ef9685fbcf4a08f756
Kwark Donghyuk <silphir@hanmail.net>
2020-06-02 18:10:06 +0900

    feat(router): use lazy loading

--- src/app/router/app.routes.tsx
+++ src/app/router/app.routes.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import Playground from '../pages/playground';
-import TestBed from '../pages/testbed';
+import styled from 'styled-components';
 
+const Playground = React.lazy(() => import('../pages/playground'));
+const TestBed = React.lazy(() => import('../pages/testbed'));
+
+const Div = styled.span`
+  margin 20px 0;
+  text-align: center;
+  display: block;
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
 function Routes() {
   return (
-    <Switch>
-      <Route exact path="/" component={Playground}/>
-      <Route path="/testbed" component={TestBed} />
-      <Route path="/temp" />
-      <Redirect path="*" to="/" />
-    </Switch>
+    <Suspense fallback={<Div>Loading...</Div>}>
+      <Switch>
+        <Route exact path="/" component={Playground} />
+        <Route path="/testbed" component={TestBed} />
+        <Route path="/temp" />
+        <Redirect path="*" to="/" />
+      </Switch>
+    </Suspense>
   );
 }
 

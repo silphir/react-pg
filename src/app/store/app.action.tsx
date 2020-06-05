1fb7fb5b0263611d4a5c03bd3b7579bd0dccd419
Kwark Donghyuk <silphir@hanmail.net>
2020-06-05 11:39:12 +0900

    refactor(redux): use creacteReducer in toolkit

--- src/app/store/app.action.tsx
+++ src/app/store/app.action.tsx
@@ -1,29 +1,14 @@
-import { createAction } from '@reduxjs/toolkit';
-import { initialState } from './app.state';
+import { createAction, createReducer } from '@reduxjs/toolkit';
+import { initialState, User } from './app.state';
 
-const setUser = createAction('SET USER', (users: any) => {
-  return {
-    payload: {
-      users
-    }
-  }
-});
+const setUser = createAction('SET USER', (users: User[]) => ({ payload: { users } }));
 
 export const userAction = {
   setUser,
 }
 
-// export const userReducer = createReducer(initialState, {
-//   setUser: (state, action) => {
-//     return { ...state, users: action.payload.users }
-//   }
-// });
-
-export function userReducer(state = initialState, action: any) {
-  switch (action.type) {
-    case setUser.type:
-      return { ...state, users: action.payload.users }
-    default:
-      return initialState
+export const userReducer = createReducer(initialState, {
+  [setUser.type]: (state, action) => {
+    return { ...state, users: action.payload.users }
   }
-}
+});

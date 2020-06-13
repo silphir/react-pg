4f20d263bb4e8642defaa9e56cd6c49b2b43481c
Kwark Donghyuk <silphir@hanmail.net>
2020-06-13 12:48:38 +0900

    feat(signup): make form multi input

--- src/app/components/form/Input.tsx
+++ src/app/components/form/Input.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export const InputForm = ({ labelText, type, value, onChange }: {
+const InputWrapper = styled.div`
+  margin-bottom: 5px;
+`;
+const InputLabel = styled.label`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1rem;
+  margin-bottom: 2px;
+  span {
+    display: inline-block;
+    width: 100%;
+    color: #0084ac;
+    font-weight: 500;
+    margin-bottom: 2px;
+  }
+  input {
+    width: 100%;
+    font-size: 1rem;
+    padding: 7px 13px;
+    border: 1px solid #999;
+    border-radius: 5px;
+  }
+`;
+const ValidationMsg = styled.span`
+  display: inline-block;
+  width: 100%;
+  color: #ed0000;
+`;
+
+export const InputForm = ({ labelText, name, type, value, validationMsg, onChange }: {
   labelText: string;
+  name: string;
   type: string;
   value: string;
+  validationMsg: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }) => {
   return (
-    <label>
-      {labelText}
-      <input type={type} value={value} onChange={onChange}></input>
-    </label>
+    <InputWrapper>
+      <InputLabel>
+        <span>{labelText}</span>
+        <input type={type} name={name} value={value} onChange={onChange}></input>
+      </InputLabel>
+      <ValidationMsg>{validationMsg}</ValidationMsg>
+    </InputWrapper>
   );
 };
--- src/app/pages/signup.tsx
+++ src/app/pages/signup.tsx
@@ -1,9 +1,66 @@
 import React, { useState } from 'react';
 import { InputForm } from '../components/form';
-import { StPageContainer, StPageHeader, StSection } from '../components/common-styled';
+import { StPageContainer, StPageHeader, StSection, StButton } from '../components/common-styled';
+import styled from 'styled-components';
+
+const FormBody = styled.div`
+  width: 25%;
+`;
 
 function SignUp() {
-  const [ name, setName ] = useState('');
+  const [ userForm, setUserForm ] = useState({
+    name: '',
+    email: ''
+  });
+
+  const [ userFormErrMsg, setUserFormErrMsg ] = useState({
+    name: '',
+    email: ''
+  });
+
+  const validate = (controlName: string, controlValue: string) => {
+    let message = '';
+    switch(controlName) {
+      case 'name':
+        message = nameValidator(controlValue);
+        break;
+      case 'email':
+        message = emailValidator(controlValue);
+        break;
+    }
+    return message;    
+  };
+
+  const nameValidator = (v: string) => {
+    if (v.length < 1) return 'invalid user name.';
+    else return '';
+  };
+
+  const emailValidator = (v: string) => {
+    // eslint-disable-next-line no-useless-escape, no-case-declarations
+    const regexr = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z]?)*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    if (!regexr.test(v)) return 'invalid email.';
+    else return '';
+  };
+
+  const validateChangeForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserForm({ ...userForm, [e.currentTarget.name]: e.currentTarget.value });
+    setUserFormErrMsg({ ...userFormErrMsg, [e.currentTarget.name]: validate(e.currentTarget.name, e.currentTarget.value) });
+  };
+
+  const validateSubmitForm = () => {
+    const resultForm = Object.entries(userForm).reduce((main, [k, v]) => ({...main, [k]: validate(k, v) }), userFormErrMsg);
+    setUserFormErrMsg(resultForm);
+
+    if (Object.values(resultForm).every(v => !v)) return true;
+    else return false;
+  };
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(validateSubmitForm()) console.log('success');
+    else console.log('failure');
+  };
 
   return (
     <StPageContainer>
@@ -11,7 +68,13 @@ function SignUp() {
         <h1>Sign Up</h1>
       </StPageHeader>
       <StSection>
-        <InputForm labelText='name' type='text' value={ name } onChange={(e) => setName(e.currentTarget.value)} />
+        <form onSubmit={submitForm}>
+        <FormBody>
+          <InputForm labelText='name' type='text' name='name' value={userForm.name} validationMsg={userFormErrMsg.name} onChange={validateChangeForm} />
+          <InputForm labelText='e-mail' type='email' name='email' value={userForm.email} validationMsg={userFormErrMsg.email} onChange={validateChangeForm} />
+        </FormBody>
+        <StButton>전송</StButton>
+        </form>
       </StSection>
     </StPageContainer>
   );

f086ce722c02032a015db30b4071ba242ef5fa82
Kwark Donghyuk <silphir@hanmail.net>
2020-06-21 16:46:02 +0900

    refactor: change component role

--- src/app/pages/userlist.tsx
+++ src/app/pages/userlist.tsx
@@ -1,17 +1,6 @@
-import React, { useEffect } from "react";
-import { Subject, EMPTY } from 'rxjs';
-import { switchMap, tap, catchError } from 'rxjs/operators';
-import { useDispatch } from "react-redux";
-import { userAction } from "../store/users.action";
+import React from "react";
 import { User } from "../store/users.state";
-import { 
-  Button,
-  PageContainer,
-  PageHeader,
-  SectionBody
-} from "../components/common-styled";
 import styled from "styled-components";
-import { restApi } from "../api/api";
 
 const Ul = styled.ul`
   list-style: none;
@@ -27,46 +16,14 @@ const Ul = styled.ul`
 function UserList (
   { users }: { users: User[] }
 ) {
-  const dispatch = useDispatch();
-  const clickEvent = new Subject<void>();
-
-  useEffect(() => {
-    const sub = clickEvent.pipe(
-      switchMap(() => restApi.getUsers().pipe(
-        tap(({ data }) => {
-          dispatch(userAction.setUser(data));
-        }),
-        catchError(({ response })=> {
-          console.log(response.message);
-          return EMPTY;
-        })
-      ))
-    ).subscribe();
-
-    return () => {
-      sub.unsubscribe();
-    };
-  // eslint-disable-next-line
-  }, []);
-  
-  const UserList = users.map((user: User, index) => (
-    <li key={index}>
-      <span>User Name : </span>{user.name}
-    </li>
-  ));
-
   return (
-    <PageContainer>
-      <PageHeader>
-        <h1>User List</h1>
-      </PageHeader>
-      <SectionBody>
-        <Button type="button" onClick={() => clickEvent.next()}> 조회 </Button>
-        <Ul>
-          { UserList }
-        </Ul>
-      </SectionBody>
-    </PageContainer>
+    <Ul>
+      {users.map((user: User, index) => (
+        <li key={index}>
+          <span>User Name: </span>{user.name}
+        </li>
+      ))}
+    </Ul>
   );
 }
 
--- src/app/pages/users.tsx
+++ src/app/pages/users.tsx
@@ -1,13 +1,53 @@
-import React from "react";
-import UserList from "./userlist";
+import React, { useEffect } from "react";
+import { Subject, EMPTY } from 'rxjs';
+import { switchMap, tap, catchError } from 'rxjs/operators';
+import { useDispatch } from "react-redux";
+import { userAction } from "../store/users.action";
+import { 
+  Button,
+  PageContainer,
+  PageHeader,
+  SectionBody
+} from "../components/common-styled";
+import { restApi } from "../api/api";
 import { useSelector } from "react-redux";
 import { selectUser } from "../store/users.state";
+import UserList from "./userlist";
 
 function Users () {
   const users = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const clickEvent = new Subject<void>();
 
+  useEffect(() => {
+    const sub = clickEvent.pipe(
+      switchMap(() => restApi.getUsers().pipe(
+        tap(({ data }) => {
+          dispatch(userAction.setUser(data));
+        }),
+        catchError(({ response })=> {
+          console.log(response.message);
+          return EMPTY;
+        })
+      ))
+    ).subscribe();
+
+    return () => {
+      sub.unsubscribe();
+    };
+  // eslint-disable-next-line
+  }, []);
+  
   return (
-    <UserList users={users} />
+    <PageContainer>
+      <PageHeader>
+        <h1>User List</h1>
+      </PageHeader>
+      <SectionBody>
+        <Button type="button" onClick={() => clickEvent.next()}> 조회 </Button>
+        <UserList users={users}></UserList>
+      </SectionBody>
+    </PageContainer>
   );
 }
 
